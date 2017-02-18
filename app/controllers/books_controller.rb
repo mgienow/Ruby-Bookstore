acb814bb488a982c80de49232d07969c97445d30
Michelle gienow <michellegienow@gmail.com>
2017-02-18 22:12:23 +0000

    First day riding the Rails

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,12 @@
+class BooksController <ApplicationController
+    def index
+        @books=Book.all
+    end
+    def show
+        @id = params[:id]
+        @book = Book.find(@id)
+    end
+    
+    def new
+    end
+end
